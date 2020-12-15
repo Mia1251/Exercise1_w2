0d7083c4e8f0f5b2d7e9c260f45e5f67f82d8583
Mia1251 <svanteomax@gmail.com>
2020-12-15 16:40:58 +0100

    Exercise1_w2

---
+++ src/main/java/se/lexicon/array/App.java
@@ -0,0 +1,18 @@
+package se.lexicon.array;
+
+public class App {
+
+    public static void main(String[] args) {
+        String[] stringArray = new String[5];
+        int[] intArray = new int[5];
+        double[] doubleArray = new double[5];
+
+        String[] numbers = new String[]{"11", "23", "39", "58", "79"};
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+        System.out.println(numbers[3]);
+        System.out.println(numbers[4]);
+
+    }
+}
